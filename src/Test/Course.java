09c02b08449edbac44151a3f8fab2e722c524e26
Administrator <Administrator@192.168.0.100>
2018-12-03 21:36:40 +0800

    home

--- src/Test/Course.java
+++ src/Test/Course.java
@@ -1,7 +1,31 @@
 package Test;
 
 public class Course {
-	private String name;
-	private String id;
+	
+	private String name;   //课程名称
+	private String id;		//课程编号
+	
+	
+	public Course() {
+		super();
+	}
+	public Course(String name, String id) {
+		super();
+		this.name = name;
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	
 	
 }
